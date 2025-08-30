924a882cb92e9d7dd166ac84a7999e8bb3714342
Shaun2006 <116490490+shaun2006@users.noreply.github.com>
2025-08-30 14:33:42 +0530

    print function
    this file will include all the prints commands that you will ever need in your life time....
    slowly slowly i will be keep on updating this file as i get time

---
+++ print.py
@@ -0,0 +1,8 @@
+# The print function in python is used to gives output to your terminal screen
+
+# this is an basic print command.
+print("hello world")
+
+# if you want a new line when you print something then you need to use \n which means new line
+print("\n hello, everyone this is a new line")
+# note:-  you can you \n at the start or end of any string
